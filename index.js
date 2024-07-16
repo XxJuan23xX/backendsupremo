e8b26b4a5beaaa5839adf31014a8c75ee1cb37a8
XxJuan23xX <manuelsima0305@gmail.com>
2024-07-16 17:42:12 -0600

    Reestructuracion del proyecto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./src/routes/userRoutes');
 
 const app = express();
 
