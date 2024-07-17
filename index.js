60d1e429ca8db9901c34247fa5da17242f4e83db
XxJuan23xX <manuelsima0305@gmail.com>
2024-07-16 18:21:51 -0600

    Reestructuracion del proyecto

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userRoutes = require('./userRoutes'); // Ajusta la ruta
+const userRoutes = require('./userRoutes'); // Asegúrate de que la ruta es correcta
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello, this is the backend!');
+});
 
 app.use('/api/users', userRoutes);
 
